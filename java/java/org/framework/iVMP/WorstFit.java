170e62b69d0ffc3b6012b28b7d8f8b762f642540
marcev93 <velazquezmarcelo@outlook.com>
2019-09-27 05:56:59 -0400

    Add files via upload

---
+++ java/java/org/framework/iVMP/WorstFit.java
@@ -0,0 +1,31 @@
+package org.framework.iVMP;
+
+import org.domain.PhysicalMachine;
+import org.domain.VirtualMachine;
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+
+public class WorstFit {
+    /**
+     * Worst Fit
+     * @param vm               VirtualMachine
+     * @param physicalMachines Physical Machines
+     * @param virtualMachines  Virtual Machines
+     * @param derivedVMs       Derived Virtual Machines
+     * @param isMigration      Indicates if the VM is being migrated
+     * @return <b>True</b>, if VM was successfully allocated
+     */
+    public static Boolean worstFit(VirtualMachine vm, List<PhysicalMachine> physicalMachines,
+                                    List<VirtualMachine> virtualMachines, List<VirtualMachine> derivedVMs, Boolean isMigration) {
+
+        return BestFit.bestOrWorstFit(false, vm, physicalMachines, virtualMachines, derivedVMs, isMigration);
+
+    }
+
+
+}
